fb200fd4649c838ae3eefb7229fd75c7511c2c4b
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-09-01 18:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   const priceElements = document.querySelectorAll(".price");
+  let total = 0;
+
+  // Calculate total
+  priceElements.forEach(priceEl => {
+    total += parseFloat(priceEl.textContent);
+  });
+	return total;
 };
 
 getSumBtn.addEventListener("click", getSum);
